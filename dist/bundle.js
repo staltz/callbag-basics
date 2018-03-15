441ee2bc3a4be513b79f885d14ade52124b02132
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2018-03-15 10:38:05 +0100

    Update dependency versions in package.json

--- dist/bundle.js
+++ dist/bundle.js
@@ -223,12 +223,57 @@ const fromIter = iter => (start, sink) => {
 module.exports = fromIter;
 
 },{}],9:[function(require,module,exports){
+/**
+ * callbag-from-obs
+ * --------------
+ *
+ * Convert an observable (or subscribable) to a callbag listenable source.
+ *
+ * `npm install callbag-from-obs`
+ *
+ * Example:
+ *
+ * Convert an RxJS Observable:
+ *
+ *     const Rx = require('rxjs');
+ *     const fromObs = require('callbag-from-obs');
+ *     const observe = require('callbag-observe');
+ *
+ *     const source = fromObs(Rx.Observable.interval(1000).take(4));
+ *
+ *     observe(x => console.log(x)(source); // 0
+ *                                          // 1
+ *                                          // 2
+ *                                          // 3
+ *
+ * Convert anything that has the `.subscribe` method:
+ *
+ *     const fromObs = require('callbag-from-obs');
+ *     const observe = require('callbag-observe');
+ *
+ *     const subscribable = {
+ *       subscribe: (observer) => {
+ *         let i = 0;
+ *         setInterval(() => observer.next(i++), 1000);
+ *       }
+ *     };
+ *
+ *     const source = fromObs(subscribable);
+ *
+ *     observe(x => console.log(x))(source); // 0
+ *                                           // 1
+ *                                           // 2
+ *                                           // 3
+ *                                           // ...
+ */
+
 const fromObs = observable => (start, sink) => {
   if (start !== 0) return;
   let dispose;
   sink(0, t => {
     if (t === 2 && dispose) {
-      dispose();
+      if (dispose.unsubscribe) dispose.unsubscribe();
+      else dispose();
     }
   });
   dispose = observable.subscribe({
@@ -286,6 +331,34 @@ const map = f => source => (start, sink) => {
 module.exports = map;
 
 },{}],13:[function(require,module,exports){
+/**
+ * callbag-merge
+ * -------------
+ *
+ * Callbag factory that merges data from multiple callbag sources. Works well
+ * with listenable sources, and while it may work for some pullable sources,
+ * it is only designed for listenable sources.
+ *
+ * `npm install callbag-merge`
+ *
+ * Example:
+ *
+ *     const interval = require('callbag-interval');
+ *     const forEach = require('callbag-for-each');
+ *     const merge = require('callbag-merge');
+ *
+ *     const source = merge(interval(100), interval(350));
+ *
+ *     forEach(x => console.log(x))(source); // 0
+ *                                           // 1
+ *                                           // 2
+ *                                           // 0
+ *                                           // 3
+ *                                           // 4
+ *                                           // 5
+ *                                           // ...
+ */
+
 function merge(...sources) {
   return (start, sink) => {
     if (start !== 0) return;
@@ -294,15 +367,16 @@ function merge(...sources) {
     let startCount = 0;
     let endCount = 0;
     const talkback = t => {
-      if (t !== 2) return;
-      for (let i = 0; i < n; i++) sourceTalkbacks[i](2);
+      if (t === 0) return;
+      for (let i = 0; i < n; i++) sourceTalkbacks[i] && sourceTalkbacks[i](t);
     };
     for (let i = 0; i < n; i++) {
       sources[i](0, (t, d) => {
         if (t === 0) {
           sourceTalkbacks[i] = d;
-          if (++startCount === n) sink(0, talkback);
+          if (++startCount === 1) sink(0, talkback);
         } else if (t === 2) {
+          sourceTalkbacks[i] = void 0;
           if (++endCount === n) sink(2);
         } else sink(t, d);
       });
